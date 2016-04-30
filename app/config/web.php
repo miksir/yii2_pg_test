42c39bf57ee14ba481a3a7935f7cd0d4124a02a8
Dmitry MiksIr <miksir@maker.ru>
2016-05-01 02:26:06 +0300

    Скелет Yii2 приложения на базе base, но почищен

---
+++ app/config/web.php
@@ -0,0 +1,49 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'yii2-pg-test',
+    'basePath' => dirname(__DIR__),
+    'vendorPath' => __DIR__ . '/../../vendor/',
+    'bootstrap' => ['log'],
+    'components' => [
+        'request' => [
+            'cookieValidationKey' => 'wqo8ty139gyhEIUUHGuhre89her9eigqe90tyiIFYGEowy4t981234hgfviuarsr082',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '/' => 'site/index'
+            ],
+        ],
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+}
+
+return $config;
